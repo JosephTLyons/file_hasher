0f420fb97ef49f527b9092030570284391421565
Joseph T Lyons <JosephTLyons@gmail.com>
2020-12-15 22:07:09 -0500

    Pushing current work

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from hashlib import sha256
 from pathlib import Path
 
 
-def get_file_path_to_sha256_dictionary(
-    directory_path, file_path_to_sha256_dictionary, should_ignore_hidden_items=True
+def get_file_path_to_sha_256_dictionary(
+    directory_path, file_path_to_sha_256_dictionary, should_ignore_hidden_items=True
 ):
     for item in directory_path.iterdir():
         if should_ignore_hidden_items and item.name.startswith("."):
@@ -17,29 +17,30 @@ def get_file_path_to_sha256_dictionary(
         if item.is_file():
             with open(item, "rb") as file:
                 bytes_string = file.read()
-                sha256_string = sha256(bytes_string).hexdigest()
-                file_path_to_sha256_dictionary[str(item)] = sha256_string
+                sha_256_string = sha256(bytes_string).hexdigest()
+                file_path_to_sha_256_dictionary[str(item)] = sha_256_string
         elif item.is_dir():
-            get_file_path_to_sha256_dictionary(item, file_path_to_sha256_dictionary)
+            get_file_path_to_sha_256_dictionary(item, file_path_to_sha_256_dictionary)
 
-    return file_path_to_sha256_dictionary
+    return file_path_to_sha_256_dictionary
 
 
-def get_sha_lines(file_path_to_sha256_dictionary):
+def get_sha_lines(file_path_to_sha_256_dictionary):
     longest_path_length = len(
         str(
             max(
-                file_path_to_sha256_dictionary.keys(), key=lambda path_object: len(str(path_object))
+                file_path_to_sha_256_dictionary.keys(),
+                key=lambda path_object: len(str(path_object)),
             )
         )
     )
 
-    number_of_items_in_dictionary = len(file_path_to_sha256_dictionary)
+    number_of_items_in_dictionary = len(file_path_to_sha_256_dictionary)
     index_padding_length = len(str(number_of_items_in_dictionary))
 
     sha_lines = []
 
-    for index, (file_path, sha_256) in enumerate(file_path_to_sha256_dictionary.items()):
+    for index, (file_path, sha_256) in enumerate(file_path_to_sha_256_dictionary.items()):
         padded_index = str(index + 1).rjust(index_padding_length)
         padded_file_name = str(file_path).ljust(longest_path_length)
 
@@ -54,9 +55,9 @@ def print_sha_lines_to_console(sha_lines):
         print(sha_line)
 
 
-def serialize_file_path_to_sha256_dictionary(file_path_to_sha256_dictionary, directory_path):
-    with open(directory_path / "file_hashes.txt", "w") as output_file:
-        json.dump(file_path_to_sha256_dictionary, output_file, indent=4)
+def serialize_file_path_to_sha_256_dictionary(file_path_to_sha_256_dictionary, output_file_path):
+    with open(output_file_path, "w") as output_file:
+        json.dump(file_path_to_sha_256_dictionary, output_file, indent=4)
 
 
 def main():
@@ -70,12 +71,15 @@ def main():
         print("Argument must be a directory path")
         sys.exit()
 
-    file_path_to_sha256_dictionary = get_file_path_to_sha256_dictionary(directory_path, {})
+    file_path_to_sha_256_dictionary = get_file_path_to_sha_256_dictionary(directory_path, {})
 
-    sha_lines = get_sha_lines(file_path_to_sha256_dictionary)
+    sha_lines = get_sha_lines(file_path_to_sha_256_dictionary)
 
-    print_sha_lines_to_console(sha_lines)
-    serialize_file_path_to_sha256_dictionary(file_path_to_sha256_dictionary, directory_path)
+    # print_sha_lines_to_console(sha_lines)
+
+    output_file_path = directory_path / "file_hashes.txt"
+
+    serialize_file_path_to_sha_256_dictionary(file_path_to_sha_256_dictionary, output_file_path)
 
 
 if __name__ == "__main__":
